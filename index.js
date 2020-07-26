ecdddaf1d41c50d0a5e54a26d43efa286e7c5fef
Evan Pavone <evanrpavone@gmail.com>
2020-07-26 15:42:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const app = "I don't do much."
-
+//39
 var dodger = document.getElementById('dodger')
 function moveDodgerLeft() {
   var leftNumbers = dodger.style.left.replace('px','')
@@ -7,5 +7,14 @@ function moveDodgerLeft() {
 
   if (left > 0) {
     dodger.style.left = `${left - 1}px`
+  }
+}
+
+function movedDodgerRight() {
+  var rightNumbers = dodger.style.right.replace('px','')
+  var right = parseInt(rightNumbers, 10)
+
+  if (right > 0) {
+    dodger.style.right = `${right - 1}px`
   }
 }
